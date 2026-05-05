6b4d7152f92cdd97a0ba14b78c409662e13f164d
Alexander Faldix <a.faldix@fz-juelich.de>
2026-05-05 11:45:42 +0200

    Removed basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/website-dev/",
+  appType: "spa",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
